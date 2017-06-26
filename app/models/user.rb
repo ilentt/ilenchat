f64ad6816809f179bda59672e5412aafd3bfa7d4
ilentt <ilentt@ilenlab.com>
2017-06-26 08:06:45 +0700

    add Message feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,19 @@
 class User < ApplicationRecord
 	has_secure_password
+	has_many :received_messages, class_name: 'Message', foreign_key: 'recipient_id'
+	has_many :sent_messages, class_name: 'Message', foreign_key: 'sender_id'
+	has_many :unread_messages, -> {where(read_at: nil)}
 	validates :email, presence: true, uniqueness:true
 	#def password=(value)
 	#	self.password_digest = Encypter.encrypt(value)
 	#end
 	def to_s
 		email
+	end
+	def received_messages
+		Message.where(sender_id: id)
+	end
+	def unread_messages
+		received_messages.where(read_at: nil)
 	end
 end
